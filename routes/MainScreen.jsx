25f0161befc9983e019678d488066e80d035c458
Clarence Rhey Salaveria <clarencerheysalaveria@plv.edu.ph>
2021-11-20 06:55:56 +0800

    [FEATURE] T-243 / Added logic to add and remove tasks with looping over all tasks

--- routes/MainScreen.jsx
+++ routes/MainScreen.jsx
@@ -1,41 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-    SafeAreaView,
-    StyleSheet,
-    View,
+    KeyboardAvoidingView,
     Text,
-    TextInput, // dont forget to lagay yung border
+    View,
+    TextInput,
+    TouchableOpacity,
+    Keyboard,
+    ScrollView,
+    SafeAreaView, Platform
 } from 'react-native';
-import { Styles, Layout } from '../components/Styles';
-import CustomButton from '../components/CustomButton';
-import {Task} from "../components/Task";
+import { Styles, Layout, MainStyle } from '../components/Styles';
+import { Task } from "../components/Task";
+
 
 export default function MainScreen() {
+    const [task, setTask] = useState();
+    const [taskItems, setTaskItems] = useState([]);
+
+    const handleAddTask = () => {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task])
+        setTask(null);
+    }
+
+    const completeTask = (index) => {
+        let itemsCopy = [...taskItems];
+        itemsCopy.splice(index, 1);
+        setTaskItems(itemsCopy)
+    }
     return (
         <SafeAreaView style={Styles.container}>
             {/* Reference at: https://www.youtube.com/watch?v=0kL6nhutjQ8 */}
-                <View style={style.header}>
-                    <Text style={style.sectionTitle}>Task List</Text>
-                    <View style={style.taskList}>
-                        <Task taskName={'Task 1'}/>
-                        <Task taskName={'Task 2'}/>
-                        <Task taskName={'Task 3'}/>
+            {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+            <ScrollView
+                contentContainerStyle={{
+                    flexGrow: 1
+                }}
+                keyboardShouldPersistTaps='handled'
+            >
+
+                {/* Today's Tasks */}
+                <View style={MainStyle.tasksWrapper}>
+                    <Text style={Styles.heading}>Today's tasks :</Text>
+                    <View style={MainStyle.items}>
+                        {
+                            taskItems.map((item, index) => {
+                                return (
+                                    <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                                        <Task text={item} />
+                                    </TouchableOpacity>
+                                )
+                            })
+                        }
                     </View>
                 </View>
+
+            </ScrollView>
+
+            {/* Write a task */}
+            {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={MainStyle.writeTaskWrapper}
+                keyboardVerticalOffset={null}
+            >
+                <TextInput style={[MainStyle.input, Styles.karla]} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+                <TouchableOpacity onPress={() => handleAddTask()}>
+                    <View style={MainStyle.addWrapper}>
+                        <Text style={[MainStyle.addText, Styles.increaseSize]}>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
-
-const style = StyleSheet.create({
-    header: {
-        paddingTop: 20,
-        paddingHorizontal: 30
-    },
-    sectionTitle: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        fontFamily: 'Rubik',
-        color: '#000',
-        marginBottom: 10
-    },
-})
